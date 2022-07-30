325fb05ae9dd7eb2f61b2186e68947d08b6f1d28
CyrixNZ <offcialcyrix@gmail.com>
2022-07-31 01:17:12 +1200

    added discord rich presence

---
+++ [ESX]/cyrix_esxshowdiscord/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cyrix'
+description 'Discord Rich Presence For ESX'
+version '1.0.0'
+
+client_script 'client/*.lua'
+
+lua54 'yes'
---
+++ [QB-Core]/cyrix_qbshowdiscord/server.lua
@@ -0,0 +1,8 @@
+
+QBCore.Functions.CreateCallback('qbshowdiscord:server:GetCurrentPlayers', function(source, cb)
+    local TotalPlayers = 0
+    for k, v in pairs(QBCore.Functions.GetPlayers()) do
+        TotalPlayers = TotalPlayers + 1
+    end
+    cb(TotalPlayers)
+end)
