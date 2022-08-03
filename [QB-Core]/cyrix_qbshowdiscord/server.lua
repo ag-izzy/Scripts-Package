9bbe7d46f3eef055ba944cbc1a0a77bfc677b590
CyrixNZ <offcialcyrix@gmail.com>
2022-08-03 16:38:05 +1200

    Update Discord-Rich-Presence

--- [QB-Core]/cyrix_qbshowdiscord/server.lua
+++
@@ -1,8 +0,0 @@
-
-QBCore.Functions.CreateCallback('qbshowdiscord:server:GetCurrentPlayers', function(source, cb)
-    local TotalPlayers = 0
-    for k, v in pairs(QBCore.Functions.GetPlayers()) do
-        TotalPlayers = TotalPlayers + 1
-    end
-    cb(TotalPlayers)
-end)
